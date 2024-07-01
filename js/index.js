99ce843b8b9902e9ee0434d9b5ccc6d04df388bb
mdurando <151706154+mdurandop@users.noreply.github.com>
2024-07-01 04:02:52 -0500

    add task function

--- js/index.js
+++ js/index.js
@@ -13,13 +13,40 @@ document.querySelector('.js-menu').addEventListener('click', () => {
 const now = dayjs().format('D MMM YYYY');
 document.querySelector('.js-now').innerText = now;
 
-const addTaskButtonElement = document.querySelector('.add-task');
-const taskContainerElement = document.querySelector('.tasks');
+const addTaskButtons = document.querySelectorAll('.add-task');
 const taskModalElement = document.querySelector('.task-modal');
 
-addTaskButtonElement.addEventListener('click', () => {
-    taskModalElement.style.display = 'flex'
-    document.querySelector('.js-discard-task').addEventListener('click', () => {
-        taskModalElement.style.display = 'none'
+addTaskButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        taskModalElement.style.display = 'flex'
+        document.querySelector('.js-discard-task').addEventListener('click', () => {
+            taskModalElement.style.display = 'none'
+        })
     })
 })
+
+const saveTaskButton = document.querySelector('.js-add-new-task');
+
+saveTaskButton.addEventListener('click', () => {
+    let taskName = document.querySelector('.task-name').value || 'Task name';
+    let taskDescription = document.querySelector('.task-description').value;
+
+    const tasksContainer = document.querySelector('.tasks');
+    const newTaskCard = document.createElement('div')
+    newTaskCard.classList.add('task-card')
+
+    newTaskCard.innerHTML = 
+    `
+        <div class="head">
+            <div class="task-name-description">
+                <p class="heading">${taskName}</p>
+                <p>${taskDescription}</p>
+            </div>
+            <img src="icons/more.svg" alt="">
+        </div>
+        <p class="date">${now}</p>
+    `
+
+    tasksContainer.appendChild(newTaskCard);
+    taskModalElement.style.display = 'none'
+});
